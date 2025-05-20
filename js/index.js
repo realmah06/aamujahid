a9a2baa41eaca5c6b93270f2ba36ae559f2e8a1d
realmah06 <154765144+realmah06@users.noreply.github.com>
2025-05-20 20:18:29 +0100

    M

---
+++ js/index.js
@@ -0,0 +1,86 @@
+  document.getElementById("loginForm").addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const number = document.getElementById("number").value.trim();
+    const password = document.getElementById("password").value.trim();
+  
+    if (number === "") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Something Went Wrong!',
+        text: 'Please fill in the Number field.',
+        cancelButtonText: 'Ok',
+        showCancelButton: true,
+        showConfirmButton: false,
+        cancelButtonColor: 'darkred'
+      });
+      return;
+    }
+  
+    if (password === "") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Something Went Wrong!',
+        text: 'Please fill in the Password field.',
+        cancelButtonText: 'Ok',
+        showCancelButton: true,
+        showConfirmButton: false,
+        cancelButtonColor: 'darkred'
+      });
+      return;
+    }
+  
+    const user = {
+      name: 'ADMIN',
+      role: 'Proprietor',
+      v_number: '07044696542',
+      v_password: 'admin'
+    };
+  
+    if (number !== user.v_number || password !== user.v_password) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Login Failed!',
+        text: 'Invalid Number or Password.',
+        cancelButtonText: 'Ok',
+        showCancelButton: true,
+        showConfirmButton: false,
+        cancelButtonColor: 'darkred'
+      });
+      return;
+    }
+  
+    Swal.fire({
+      icon: 'question',
+      title: 'Login to System?',
+      showConfirmButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Login',
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#276dc9',
+      cancelButtonColor: 'darkred',
+      allowOutsideClick: false,
+      preConfirm: () => {
+        return new Promise((resolve) => {
+          Swal.showLoading();
+          setTimeout(() => {
+            resolve();
+          }, 2000);
+        });
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Congratulations',
+          text: 'Welcome back to ADMIN Dashboard',
+          timer: 3000,
+          showConfirmButton: true,
+          confirmButtonColor: '#276dc9',
+        }).then(() => {
+          window.location.href = 'dashboard.html';
+        });
+      }
+    });
+  });
+  
